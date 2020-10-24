241cdcacc5f627893aecb9a9b9be25b92bb34529
SoftwareKater <softwarekater@gmail.com>
2020-10-24 19:23:04 +0200

    add board component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GameBoardModule } from './game-board/game-board.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    GameBoardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game-board/game-board.module.ts
+++ src/app/game-board/game-board.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TileComponent } from './tile/tile.component';
+import { BoardComponent } from './board/board.component';
 
 
 
 @NgModule({
-  declarations: [TileComponent],
+  declarations: [TileComponent, BoardComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [BoardComponent]
 })
 export class GameBoardModule { }
