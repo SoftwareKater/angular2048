b61b6b7c3247c1c586a2b6e31095114a9a8fe6ce
SoftwareKater <softwarekater@gmail.com>
2020-10-25 22:03:06 +0100

    fix: vertical swipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  HammerModule,
+  HAMMER_GESTURE_CONFIG,
+  HammerGestureConfig,
+} from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import * as Hammer from 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { GameBoardModule } from './game-board/game-board.module';
-import { GameUiModule } from './game-ui/game-ui.module';
 import { GameModule } from './game/game.module';
 
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = {
+    swipe: { direction: Hammer.DIRECTION_ALL },
+  };
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     GameModule,
+    HammerModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HAMMER_GESTURE_CONFIG,
+      useClass: MyHammerConfig,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
