d1d7a4041475f685e33faf4c9d725dbd6b13f78f
SoftwareKater <softwarekater@gmail.com>
2020-12-05 11:52:08 +0100

    add angular/pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   template: `<app-main></app-main>`,
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(
+    private readonly swUpdate: SwUpdate,
+  ) {
+    this.setupUpdates();
+  }
+
+  setupUpdates() {
+    this.swUpdate.available.subscribe(update => {
+      this.swUpdate.activateUpdate().then(event => {
+        console.log('Application has been updated. New version will be available after reloading the page.');
+      });
+    });
+
+    this.swUpdate.checkForUpdate();
+  }
+
+}
